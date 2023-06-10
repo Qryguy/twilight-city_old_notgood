fbc2f698a71a47f7e09f607defc2a08d2c6db8c6
Q <sheeranrye@gmail.com>
2023-06-10 18:51:57 -0500

    Basic setup - Two units and Test Level

---
+++ scripts/playerController.gd
@@ -0,0 +1,44 @@
+extends CharacterBody2D
+#probably should have just made these separate scripts but whatever
+@export var unitName: String
+@export var speed = 300.0
+@export var health = 1
+
+var playerControl: String
+
+
+func _ready():
+	#set unit controls and settings, set brawn as default? to avoid annoying errors
+	if len(unitName) == 0:
+		unitName = "brawn"
+	if unitName == "brawn":
+		playerControl = "player1"
+	if unitName == "brain":
+		playerControl = "player2"
+	print(playerControl)
+
+#process for standard inputs, actually should probably change this to some kind of collider?
+func _process(delta):
+	_use(unitName)
+
+#physics for movement
+func _physics_process(delta):
+	##use delta for time based 
+	var xDirection = Input.get_axis(playerControl+"_left", playerControl+"_right")
+	if xDirection:
+		velocity.x = xDirection * speed
+	else:
+		velocity.x = move_toward(velocity.x, 0, speed)
+
+	var yDirection = Input.get_axis(playerControl+"_up", playerControl+"_down")
+	if yDirection:
+		velocity.y = yDirection * speed
+	else:
+		velocity.y = move_toward(velocity.y, 0, speed)
+
+	move_and_slide() #seems to apply the actual movement
+
+func _use(unit):
+	if unit == "":
+		pass
+	
